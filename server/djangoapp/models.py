6c9e4935df00ab0cccec8b98997a825f3affb2ad
Sean-Murphy98 <seangm4.murphy@gmail.com>
2024-05-06 18:20:43 -0400

    Django proxy services of Backend APIs

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,23 +1,34 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
 
+    def __str__(self):
+        return self.name
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    test = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    dealerId = models.IntegerField(blank = False)
+    type = models.CharField(max_length=10,default = "SUV", choices={"Sedan":"Sedan","SUV":"SUV","Wagon":"Wagon"})
+    year = models.IntegerField(default=2023, 
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ])
+    engineSize = models.DecimalField(max_digits=4, decimal_places=1)
+
+    def __str__(self):
+        return "Make " + self.car_make.name + "model " + self.name + "type " + self.type
 # - Type (CharField with a choices argument to provide limited choices
 # such as Sedan, SUV, WAGON, etc.)
 # - Year (IntegerField) with min value 2015 and max value 2023
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,7 +12,9 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
 
     #path for logour
-    path(route='logout', view=views.logout_request, name='logout')
+    path(route='logout', view=views.logout_request, name='logout'),
+    
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
 
     # path for add a review view
