8885ba4714712c7ca600e5993bc2752563799f7f
Sean-Murphy98 <92535432+Sean-Murphy98@users.noreply.github.com>
2024-05-08 11:48:40 +0100

    Update models.py linting

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-#from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -20,18 +20,19 @@ class CarModel(models.Model):
     test = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     dealerId = models.IntegerField(blank=False)
-    type = models.CharField(max_length=10, default="SUV", 
-        choices={"Sedan": "Sedan", "SUV": "SUV", "Wagon": "Wagon"})
+    type = models.CharField(max_length=10, default="SUV",
+            choices={"Sedan": "Sedan", "SUV": "SUV", "Wagon": "Wagon"})
     year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
-    engineSize = models.DecimalField(max_digits=4, 
+            validators=[
+                MaxValueValidator(2023),
+                MinValueValidator(2015)
+            ])
+    engineSize = models.DecimalField(max_digits=4,
                                      decimal_places=1)
 
     def __str__(self):
-        return "Make " + self.car_make.name + "model " + self.name + "type " + self.type
+        return "Make " + self.car_make.name + "model " + 
+                self.name + "type " + self.type
 # - Type (CharField with a choices argument to provide limited choices
 # such as Sedan, SUV, WAGON, etc.)
 # - Year (IntegerField) with min value 2015 and max value 2023
